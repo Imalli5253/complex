cc2621ce3c553c14f31d1cdf810a3acc42c8c787
malli86271 <pnr.759@gmail.com>
2019-04-24 12:28:56 +0530

    Updated Fib Class

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -51,16 +51,18 @@ class Fib extends Component {
                 <div>
                     <form onSubmit={this.handleSubmit}>
                     <label> Enter your index</label>
-                    <input 
+                    <input
                      value={this.state.index} onChange={event => this.setState({ index: event.target.value })}/>
                     <button>Submit</button>
                     </form>
                     <h3> Indexes I have seen</h3>
-                    {render.seenIndexes()}
+                    {this.renderSeenIndexes()}
 
-                    <h3> Calculated Values</h3>       
+                    <h3> Calculated Values</h3>
+                    {this.renderValues()}
                 </div>
             );
         }
+
 }
 export default Fib;
